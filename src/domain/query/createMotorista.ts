ffa3d9ca9d4d20409b541a0b383aa30621b1ef7c
Paulo Hercílio <paulo.hercilio2015@gmail.com>
2022-10-13 11:41:19 -0300

    atualizando lista de motoristas ao final do cadastro

--- src/domain/query/createMotorista.ts
+++ src/domain/query/createMotorista.ts
@@ -2,7 +2,7 @@ import { Motorista } from "@prisma/client";
 import axios, { AxiosResponse } from "axios";
 import { useQuery, UseQueryResult } from "react-query";
 
-interface ICreateMotorista {
+interface ICreateMotoristaDTO {
     nome: string;
     celular: string;
     telefone: string;
@@ -12,12 +12,20 @@ interface ICreateMotorista {
     vinculo: string;
 }
 
-function createMotorista(params: ICreateMotorista): UseQueryResult<Motorista> {
+interface ICreateMotoristaResponse {
+    motorista: Motorista;
+}
+
+function createMotorista(params: ICreateMotoristaDTO, onSuccess: (data: ICreateMotoristaResponse) => void): UseQueryResult<ICreateMotoristaResponse> {
     return useQuery('createMotorista', async () => {
-        const response: AxiosResponse = await axios.post("http://localhost:3000/api/motoristas/create", 
-        { ...params });
-        return response;
-    }, { enabled: false });
+        const { data }: AxiosResponse = await axios.post("http://localhost:3000/api/motoristas/create",
+            { ...params });
+        return data;
+    }, {
+        enabled: false,
+        onSuccess
+    });
 }
 
 export { createMotorista };
+export type { ICreateMotoristaDTO, ICreateMotoristaResponse }
--- src/domain/store/motoristas.ts
+++ src/domain/store/motoristas.ts
@@ -4,16 +4,19 @@ import { Motorista } from '@prisma/client';
 export interface IMotoristasStore {
     motoristas: Motorista[] | [];
     setMotoristas: (motoristas: Motorista[]) => void;
+    addMotorista: (motorista: Motorista) => void;
 }
 
 
 export const initialMotoristasStoreState: IMotoristasStore = {
     motoristas: [],
     setMotoristas: (motoristas: Motorista[]) => { },
+    addMotorista: (motorista: Motorista) => {}
 }
 
 
 export const createMotoristasStore = (set: any, get: any, api: any) => ({
     motoristas: [],
     setMotoristas: (motoristas: Motorista[]) => set({ motoristas }),
+    addMotorista: (motorista: Motorista) => set((state: IMotoristasStore) => ({ motoristas: [...state.motoristas, motorista] }))
 })
