7c286c1cdb60f3613bbb4071d1f0673d9be0b1ea
Paulo Hercílio <paulo.hercilio2015@gmail.com>
2022-10-13 11:25:53 -0300

    adcionando rota de busca de motoristas

--- src/pages/motoristas.tsx
+++ src/pages/motoristas.tsx
@@ -1,9 +1,11 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { Button, Container } from "react-bootstrap";
 import NavBarT from "../presentation/components/NavBar";
 import ListaMotoristas from "../presentation/components/Listas/ListaMotoristas";
 import CadastroMotorista from "../presentation/components/Cadastros/CadastroMotorista";
 import style from "../presentation/components/Cadastros/CadastroLocadora/index.module.scss"
+import axios from "axios";
+import { initializeStore } from "../domain/store/store";
 
 
 const Teste: NextPage = () => {
@@ -17,6 +19,34 @@ const Teste: NextPage = () => {
       </Container>
       </>
     )
+};
+
+export const getServerSideProps: GetServerSideProps = async context => {
+    const zustandStore = initializeStore();
+
+    const state = zustandStore.getState();
+
+    const { verifySession } = state;
+
+    const  isAuthenticated = await verifySession(context);
+
+    if (!isAuthenticated) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+    const motoristas = await axios.get("http://localhost:3000/api/motoristas");
+
+    state.motoristas = motoristas.data.motoristas;
+
+    return {
+        props: {
+            initialZustandState: JSON.parse(JSON.stringify(state)),
+        }
+    }
 }
 
 export default Teste
