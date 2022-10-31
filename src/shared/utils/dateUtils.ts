a557f75f56ad6134fed2d0de448ca0266be75087
treepo1 <thiago.freire45@gmail.com>
2022-10-31 08:51:22 -0300

    retornando data

--- src/shared/utils/dateUtils.ts
+++ src/shared/utils/dateUtils.ts
@@ -1,7 +1,7 @@
 import dayjs from "dayjs"
 
 
-export const getFormatedDateFromString = (date: string) => {let parsedDate = date.split('/'); dayjs(`${parsedDate[2]}-${parsedDate[1]}-${parsedDate[0]}`).locale('pt-br').toDate();}
+export const getFormatedDateFromString = (date: string) => {let parsedDate = date.split('/'); return dayjs(`${parsedDate[2]}-${parsedDate[1]}-${parsedDate[0]}`).locale('pt-br').toDate();}
 export const getFormatedDateTimeString = (date: Date) => dayjs(date).locale('pt-br').format('DD/MM/YYYY-HH:mm');
 export const getFormatedDateString = (date: Date) => dayjs(date).locale('pt-br').format('DD/MM/YYYY')
 
