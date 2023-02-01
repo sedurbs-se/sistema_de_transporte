1ee99f73a4a6ae576185871f13b2cab9b49a3fa4
Paulo Hercilio Araujo Santana <phasantana@sedurbs.se>
2023-02-01 09:01:18 -0300

    resolvendo problema de horario em cadastro Solicitacao,movimentacao

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+
+
+# git pull 
+
+# git remote set-url origin https://Kertf22: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@github.com/sedurbs-se/sistema_de_transporte
+
+git pull 
+
+npm run build
+
+pm2 restart transporte
