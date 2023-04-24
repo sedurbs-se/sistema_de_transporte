106a4e81182488081a73f5e6958f45baccf9b068
Paulo Hercilio Araujo Santana <phasantana@sedurbs.se>
2023-04-24 10:50:30 -0300

    fix: Permitir editar Solicitação após ela ser aprovada

--- src/http/modules/solicitacao/controller/listSolicitacaoController.ts
+++ src/http/modules/solicitacao/controller/listSolicitacaoController.ts
@@ -1,64 +1,70 @@
-
-
 import { Request, Response } from "src/http/type";
 import catchAsyncErrors from "../../../middlewares/catchAsyncErrors";
 import prisma from "../../../../shared/prisma.index";
 
-
 // get one note from with a note id request dynamically
-const listSolicitacaoController = catchAsyncErrors(async (req: Request, res: Response) => {
-
+const listSolicitacaoController = catchAsyncErrors(
+  async (req: Request, res: Response) => {
     const { page, limit } = req.query;
 
     const solicitacoes = await prisma.solicitacao.findMany({
-        where: {
-            statussolicitacao: {
-                nome: {
-                    notIn: ["CANCELADO", "AUTORIZADO"]
-                 }
-            }
+      where: {
+        statussolicitacao: {
+          nome: {
+            notIn: ["CANCELADO"],
+          },
         },
-        include: {
-            municipiosolicitacao: {
-                select: {
-                    municipio: {
-                        select: {
-                            nome: true,
-                            id: true
-                        }
-                    }
-                }
-            },
-            statussolicitacao: {
-                select: {
-                    nome: true,
-                    id: true
-                }
-            },
-            tiposolicitacao: {
-                select: {
-                    nome: true,
-                    id: true
-                }
-            }
-            
-        },
-        orderBy: {
-            data_hora_saida: 'asc'
+        // Pegar somenete aquele sem movimentacao
+
+        Movimentacao: {
+            is: null
         }
-    })
+      },
+      
+      include: {
+        municipiosolicitacao: {
+          select: {
+            municipio: {
+              select: {
+                nome: true,
+                id: true,
+              },
+            },
+          },
+        },
+        statussolicitacao: {
+          select: {
+            nome: true,
+            id: true,
+          },
+        },
+        tiposolicitacao: {
+          select: {
+            nome: true,
+            id: true,
+          },
+        },
+
+      },
+      orderBy: {
+        data_hora_saida: "asc",
+      },
+    });
 
 
-    const mappedSolicitacoes = solicitacoes.map(solicitacao => {
-        return {
-            ...solicitacao,
-            municipiosolicitacao: solicitacao.municipiosolicitacao.map(municipio => municipio.municipio)
-        }
-    })
+    const mappedSolicitacoes = solicitacoes.map((solicitacao) => {
+      return {
+        ...solicitacao,
+        municipiosolicitacao: solicitacao.municipiosolicitacao.map(
+          (municipio) => municipio.municipio
+        ),
+      };
+    });
 
     res.status(200).json({
-        solicitacoes: mappedSolicitacoes
+      solicitacoes: mappedSolicitacoes,
     });
-});
+  }
+);
 
-export { listSolicitacaoController }
+export { listSolicitacaoController };
