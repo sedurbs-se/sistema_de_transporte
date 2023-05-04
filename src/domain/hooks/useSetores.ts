d2d361d50bcfd6ecebdba43693535b8f19eabc8d
Thiago Freire de Carvalho <tfcarvalho@sedurbs.se>
2023-05-04 09:37:37 -0300

    fix: setores em relatorio

--- src/domain/hooks/useSetores.ts
+++ src/domain/hooks/useSetores.ts
@@ -4,13 +4,21 @@ import { removeModal, setLoading } from "@shared/utils/modalUtils";
 import { useEffect } from "react";
 
 const useSetores = (page?: number) => {
-  const { setSetores, setSetorPages } = useStore((state) => state);
+  const { setSetores, setSetorPages, setSetoresSearch } = useStore((state) => state);
 
   const fetch = async () => {
     setLoading()
-    const { setores, total } = page ? await fetchSetores({ page, limit: 10 }) : await fetchSetores({ all: 1 });
-    setSetores(setores);
-    setSetorPages(total);
+    if(page) {
+      const { setores, total } = await fetchSetores({ page, limit: 10 })
+      setSetores(setores);
+      setSetorPages(total);
+    }
+    else {
+      const { setores } = await fetchSetores({});
+      setSetoresSearch(setores);
+    }
+
+   
     removeModal()
   };
 
